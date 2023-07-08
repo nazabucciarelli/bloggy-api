94b66f611acce7f1b138712da44d5bb779685cba
nazabucciarelli <bnazareno03@gmail.com>
2023-07-07 23:07:24 -0300

    great changes on UI

--- app.py
+++ app.py
@@ -74,6 +74,10 @@ def login():
     return render_template("login.html")
 
 
-@app.route("/register")
+@app.route("/register") 
 def register():
     return render_template("register.html")
+
+@app.route("/post", methods=['POST'])
+def post():
+    pass  
