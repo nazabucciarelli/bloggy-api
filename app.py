b4d398da9c0f5f7f81657865c359123ae00178c7
nazabucciarelli <bnazareno03@gmail.com>
2023-07-17 01:55:37 -0300

    all perfectly working and finished

--- app.py
+++ app.py
@@ -20,8 +20,7 @@ class User(db.Model):
     email = db.Column(db.String(100), nullable=False)
     username = db.Column(db.String(30), nullable=False)
     password = db.Column(db.String(20), nullable=False)
-    visible = db.Column(db.Boolean,default=True,nullable=False)
-
+    visible = db.Column(db.Boolean, default=True, nullable=False)
 
     def __str__(self) -> str:
         return self.username
@@ -32,8 +31,7 @@ class Category(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False)
-    visible = db.Column(db.Boolean,default=True,nullable=False)
-
+    visible = db.Column(db.Boolean, default=True, nullable=False)
 
     def __str__(self) -> str:
         return self.name
@@ -53,7 +51,7 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, ForeignKey("user.id"), nullable=False)
     category_id = db.Column(db.Integer, ForeignKey(
         "category.id"), nullable=False)
-    visible = db.Column(db.Boolean,default=True,nullable=False)
+    visible = db.Column(db.Boolean, default=True, nullable=False)
 
     def __str__(self) -> str:
         return self.title
@@ -70,8 +68,7 @@ class Commentary(db.Model):
                           onupdate=func.now(), nullable=True)
     user_id = db.Column(db.Integer, ForeignKey("user.id"), nullable=False)
     post_id = db.Column(db.Integer, ForeignKey("post.id"), nullable=False)
-    visible = db.Column(db.Boolean,default=True,nullable=False)
-
+    visible = db.Column(db.Boolean, default=True, nullable=False)
 
     def __str__(self) -> str:
         return self.content
@@ -91,9 +88,11 @@ def login():
 def register():
     return render_template("register.html")
 
+
 @app.route("/modal_edit")
 def modal_edit():
     return render_template("modal_edit.html")
+
 
 @app.route("/post/<category_id>", methods=['POST'])
 def post(category_id):
@@ -104,7 +103,7 @@ def post(category_id):
     post_date = func.current_timestamp()
     post_edit_date = None
     new_post = Post(title=post_title, content=post_content, date=post_date, edit_date=post_edit_date,
-                    user_id=post_user_id, category_id=post_category_id,visible=1)
+                    user_id=post_user_id, category_id=post_category_id, visible=1)
     db.session.add(new_post)
     db.session.commit()
     return redirect(url_for('inject_posts', category_id=post_category_id))
@@ -117,7 +116,7 @@ def comment(post_id):
     comment_edit_date = None
     comment_user_id = request.form['comment_user_id']
     new_comment = Commentary(content=comment_content, date=comment_date,
-                             edit_date=comment_edit_date, user_id=comment_user_id, post_id=post_id,visible=1)
+                             edit_date=comment_edit_date, user_id=comment_user_id, post_id=post_id, visible=1)
     db.session.add(new_comment)
     db.session.commit()
     category_id = db.session.query(Post).get(post_id).category_id
@@ -141,7 +140,7 @@ def register_user():
         User.username == register_username).count() > 0
     if not username_exists:
         new_user = User(email=register_email,
-                        username=register_username, password=register_password,visible=1)
+                        username=register_username, password=register_password, visible=1)
         db.session.add(new_user)
         db.session.commit()
         success = True
@@ -162,6 +161,25 @@ def login_user():
         pass
     return render_template('login.html', success=success)
 
+
+@app.route("/edit_post/<post_id>", methods=['POST'])
+def edit_post(post_id):
+    post = db.session.query(Post).get(post_id)    
+    post.title = request.form[f'new_title_post{post_id}']
+    post.content = request.form[f'new_content_post{post_id}']
+    post.edit_date = func.current_timestamp()
+    db.session.commit()
+    return inject_posts(post.category_id)
+
+@app.route("/edit_comment/<comment_id>", methods=['POST'])
+def edit_comment(comment_id):
+    comment = db.session.query(Commentary).get(comment_id)    
+    comment.content = request.form[f'new_content_comment{comment_id}']
+    comment.edit_date = func.current_timestamp()
+    post = db.session.query(Post).get(comment.post_id)
+    db.session.commit()
+    return inject_posts(post.category_id)
+
 @app.context_processor
 def inject_users():
     users = db.session.query(User).all()
@@ -180,7 +198,7 @@ def inject_categories():
     return dict(categories=categories)
 
 
-@app.route("/delete_post/<post_id>",methods=['POST'])
+@app.route("/delete_post/<post_id>", methods=['POST'])
 def delete_post(post_id):
     post = db.session.query(Post).get(post_id)
     post.visible = 0
@@ -190,9 +208,9 @@ def delete_post(post_id):
 
 @app.route("/delete_comment/<comment_id>", methods=['POST'])
 def delete_comment(comment_id):
-    comment = db.session.query(Commentary).filter(Commentary.id == comment_id).first()
+    comment = db.session.query(Commentary).filter(
+        Commentary.id == comment_id).first()
     comment.visible = 0
-    post = db.session.query(Post).filter(Post.id == comment.post_id ).first()
+    post = db.session.query(Post).filter(Post.id == comment.post_id).first()
     db.session.commit()
     return inject_posts(post.category_id)
-
